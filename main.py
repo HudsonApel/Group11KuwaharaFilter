9710d540dc0731fc84b036e8d7217a21ef358336
Hudson Apel <apl1of2@gmail.com>
2023-11-08 16:47:48 -0600

    Updated main.py, currently will not work
    Refactored some code to and implemented some of the parts, need to implement U[i], V[i], and final output and then it should work.

--- main.py
+++ main.py
@@ -16,32 +16,42 @@ def Kuwahara_filter( # from section 3
     # convert into grayscale into float for computation
     img = img / 255
     img = img.astype("float32")
+    diameter = int(r*2 + 1)
     
-    """
     #old code that zero padded border, don't know if we will do zero padding
-    half_size = r
+    half_size = int(r)
     img_filtered = np.zeros(img.shape) # Placeholder of the filtered image
     #zero-padding
     img = cv2.copyMakeBorder(img, half_size, half_size, half_size, half_size, cv2.BORDER_CONSTANT, 0);
-    """
+    
     sizeX, sizeY = img.shape
     
-    g_kernel = linear_filter.gauss_kernel_generator(int(r*2), sigma)
+    g_kernel = linear_filter.gauss_kernel_generator(diameter, sigma)
+    g_kernel_over4 = linear_filter.gauss_kernel_generator(diameter, sigma / 4.0)
+    print(g_kernel)
+    print(g_kernel_over4)
+    
+    # precompute V[i] (7) and w[i] (8) according to N and r
+    w = np.ndarray((N, diameter, diameter))
+    for i in range(0, N):
+        # TODO: U[i] = ...
+        # TODO: V[i] = ...
+        w[i] = np.dot(g_kernel, V[i])
     
     #loop through image, and for each,
-    #compute cutting function Vi, (7)
-    #compute weighting function wi, (8)
-    #compute weighted local average mi, (5)
-    #compute si^2 (5)
-    #compute final output (11)
+    for x in range(half_size, sizeX - half_size):
+        for y in range(half_size, sizeY - half_size):
+            m = np.zeros(N, dtype=float)
+            s2 = np.zeros(N, dtype=float)
+            for i in range(0, N):
+                #compute weighted local average mi, (5)
+                img_window = img[x-half_size:x+half_size+1, y-half_size:y+half_size+1]
+                m[i] = np.sum(img_window * w[i])
+                #compute si^2 (5)
+                s2[i] = np.sum(np.square(img_window) * w[i]) - m[i]**2
+            # TODO: compute final output (11)
+            #img_filtered[x - half_size, y - half_size] = output based on filtering at img[i, j]
     
-    
-    """
-    #old code to iterate through the zero padded matrix
-    for i in range(half_size, sizeX - half_size):
-        for j in range(half_size, sizeY - half_size):
-            img_filtered[i - half_size, j - half_size] = output based on filtering at img[i, j]
-    """
     
     # convert back into grayscale
     img_filtered = img # temporary code
@@ -62,36 +72,40 @@ if __name__ == '__main__':
     # TODO: all parameters will need to be tweaked
     output = Kuwahara_filter(gray, 8, 3, 5.0, 4.0)
     
-    # visualization for debugging
-    fig = plt.figure()
     
-    # show input image
-    ax = fig.add_subplot(1, 2, 1)
-    # plt.imshow(original)
-    ax.set_title('input image')
-    
-    # show output image
-    ax = fig.add_subplot(1, 2, 2)
-    # plt.imshow(output, cmap="gray")
-    ax.set_title('resulting image')
-
-    # plt.show()
-    cv2.imwrite('results_kuwahara.png', output)
 
     # Gaussian filtering
-    reduced_image = cv2.resize(gray, (256, 256), interpolation=cv2.INTER_AREA)
+    #reduced_image = cv2.resize(gray, (256, 256), interpolation=cv2.INTER_AREA)
     kernel_size = 7
     spatial_var = 15  # sigma_s^2
     gaussian_filter = linear_filter.gauss_kernel_generator(kernel_size, spatial_var)
     # normalization term
     gaussian_filter_normalized = gaussian_filter / (np.sum(gaussian_filter) + 1e-16)
     # apply the filter to process the image: im
-    gaussian_image = linear_filter.linear_local_filtering(reduced_image, gaussian_filter_normalized)
+    gaussian_image = linear_filter.linear_local_filtering(gray, gaussian_filter_normalized)
     cv2.imwrite('result_gaussian.png', gaussian_image)
+    
+    # visualization for debugging
+    fig = plt.figure()
+    
+    # show input image
+    ax = fig.add_subplot(1, 2, 1)
+    plt.imshow(original)
+    ax.set_title('input image')
+    
+    # show output image
+    ax = fig.add_subplot(1, 2, 2)
+    plt.imshow(output, cmap="gray")
+    ax.set_title('resulting image')
 
+    plt.show()
+    cv2.imwrite('results_kuwahara.png', output)
+
+"""
     # Bilateral filtering
     spatial_variance = 30
     intensity_variance = 0.5
     kernel_size = 7
     bilateral_image = bilateral_filter.bilateral_filtering(gray, spatial_variance, intensity_variance, kernel_size)
     cv2.imwrite('results_bilateral.png', bilateral_image)
+"""

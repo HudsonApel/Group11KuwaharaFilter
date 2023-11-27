Fixed overflow issues and cleaned up code

main.py
@@ -13,9 +13,6 @@ def Kuwahara_filter( # from section 3
     q: float, # parameter, controls the sharpness of transition
 ) -> np.uint8:
 
-    # just for debuging, delete later
-    original = img
-
     # convert into grayscale into float for computation
     img = img / 255
     img = img.astype("float32")
@@ -44,28 +41,16 @@ def Kuwahara_filter( # from section 3
             i = int(N * (radians / (2 * math.pi)))
             U[i, y, x] = N
 
-    #V is temporary var checking sum of Vi
-    V = 0
     arr = np.zeros((N))
+    middle = U[0, diameter // 2, diameter // 2]
     for i in range(0, N):
+        U[i, diameter // 2, diameter // 2] = middle / N
         Vi = U[i] * g_kernel_over4
-        V += np.sum(Vi)
         w[i] = g_kernel * U[i]
         arr[i] = np.sum(w[i]) / N
 
-    #check, temporary code
-    print("\ntesting gaussian kernels, should be about 1.00, if not try temporarily increasing radius size to see if it approaches 1")
-    print("sum of gaussian = " + str(np.sum(g_kernel)) )
-    print("sum of gaussian_over_4 = " + str(np.sum(g_kernel_over4)))
-    print("sum of Ui = " + str(np.sum(U)) + ", should be = " + str(diameter*diameter*N))
-
-    print("\n\nThe following are the important ones, if Vi is off try setting sigma closer to the size of the radius")
-    print("sum of Vi = " + str(V) + ", should be = " + str(N))
-    print(str(np.sum(abs((sum(w) / N) - g_kernel))) + ", diff btw (sum of wi) / N vs. gaussian kernel, should be 0.0")
-    print("the following array measures how close the final sums are to expected answer, so this difference should approach 0.00")
-    print(arr - (1.0 / N))
-
-    #check, temporary code
+    """
+    #check, shows the cutting and weighting functions based on the Parameters
     for i in range(0, N):
         imgcheck = U[i] * 255
         imgcheck = np.uint8(imgcheck)
@@ -73,6 +58,7 @@ def Kuwahara_filter( # from section 3
         imgcheck = w[i] * 255 / np.max(w)
         imgcheck = np.uint8(imgcheck)
         cv2.imwrite(("wi_check" + str(i) + ".png"), imgcheck)
+        """
 
     #loop through image, and for each,
     for x in range(half_size, sizeX - half_size):
@@ -86,10 +72,11 @@ def Kuwahara_filter( # from section 3
                 img_window = img[x-half_size:x+half_size, y-half_size:y+half_size]
                 m[i] = np.sum(img_window * w[i])
                 # compute si^2 (5)
-                s2[i] = np.sum(np.square(img_window) * w[i]) - m[i]**2
-                if m[i] != 0 and s2[i] != 0:
-                    top += m[i] * s2[i]**(-q)
-                    bottom += s2[i]**(-q)
+                #s2[i] = np.sum(np.matmul(img_window, img_window) * w[i]) - (m[i] * m[i])
+                s2[i] = np.sum(np.square(img_window) * w[i]) - (m[i] * m[i])
+                s2[i] = math.sqrt(max(s2[i], 0.0000000000000001)) # sqrt because we use si not s^2i
+                top += m[i] * math.pow(s2[i], -q)
+                bottom += math.pow(s2[i], -q)
             # compute final output (11)
             if bottom != 0:
                 img_filtered[x - half_size, y - half_size] = top / bottom
@@ -98,36 +85,25 @@ def Kuwahara_filter( # from section 3
 
     # convert back into grayscale
     img_filtered = img_filtered * 255
-
-    # temporary, error is happening especially when q > 2, then goes below zero and above 255
-    print("max = ")
-    print(np.max(img_filtered))
-    print(np.max(original))
-    print("min = ")
-    print(np.min(img_filtered))
-    print(np.min(original))
-
-
     img_filtered = np.uint8(img_filtered)
     return img_filtered
 
 # main function
 if __name__ == '__main__':
     # read the rgb image
-    rgb_filename = 'image3.png'
+    rgb_filename = 'image.jpg'
     im = cv2.imread(rgb_filename)
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     cv2.imwrite('grayscale.png', gray)
 
-    # TODO: all parameters will need to be tweaked, probably depending on photo
     # Kuwahara Parameters
     N = 8 # number of sectors
     sigma = 6.0 # sigma, ussually good between r and r/2
-    r = 11.0 # radius of the filter
-    q = 2 # sharpness
+    r = 9.0 # radius of the filter
+    q = 5
 
     # For comparing Bilateral filtering
-    intensity_variance = 2.0
+    intensity_variance = 0.3
 
     # Kuwahara filter
     output = Kuwahara_filter(gray, N, sigma, r, q)